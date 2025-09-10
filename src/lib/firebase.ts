3436b55ab7533aab01b0a2441c1e2dab8d7bc5be
Manuel Oppong <manueloppong14@gmail.com>
2025-09-10 02:22:59 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+
+import { initializeApp, getApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+  "projectId": "bakesnplates",
+  "appId": "1:661416198704:web:f330ae9e730c43f3f5a3f5",
+  "storageBucket": "bakesnplates.appspot.com",
+  "apiKey": "AIzaSyD55cqczaXiRz24v0a2Dg2O8Srqv3bItlw",
+  "authDomain": "bakesnplates.firebaseapp.com",
+  "messagingSenderId": "661416198704"
+};
+
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
